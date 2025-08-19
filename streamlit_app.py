cbbffda7be596e62ba9b461760bc158eeaf61320
Etoufumina <ehan7666.01futs64duycutfug@gmail.com>
2025-08-19 01:10:13 +0000

    nya-

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import  streamlit as st
-import spacy # type: ignore
+import spacy # pyright: ignore[reportMissingImports]
 
 # spaCyの英語モデルを読み込む
 nlp = spacy.load("en_core_web_sm")
